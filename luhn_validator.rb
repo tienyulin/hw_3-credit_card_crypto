d1ab6d0c746af8bf4e3beee64b187e4abdcab6be
tienyulin <yh50114@gmail.com>
2021-03-13 20:05:37 +0800

    feat(validator): implement luhn validator

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -7,5 +7,14 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+
+    odd_index_num = nums_a.reverse_each.select.with_index { |_, i| i.even? }
+    odd_total = odd_index_num.sum
+
+    even_index_num = nums_a.reverse_each.select.with_index { |_, i| i.odd? }
+    even_index_num = even_index_num.map { |n| n * 2 > 9 ? n * 2 - 9 : n * 2 }
+    even_total = even_index_num.sum
+
+    valid = (odd_total + even_total) % 10 == 0 ? true : false
   end
 end
