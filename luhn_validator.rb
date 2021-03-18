9ae0b98bea3c466ecbaabf683f1a5b71acf77d48
tienyulin <yh50114@gmail.com>
2021-03-18 15:48:16 +0800

    feat(luhn): split code to more function

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -9,13 +9,25 @@ module LuhnValidator
 
     # TODO: use the integers in nums_a to validate its last check digit
 
-    odd_index_num = nums_a.reverse_each.select.with_index { |_, i| i.even? }
-    odd_total = odd_index_num.sum
+    odd_total = get_odd_total(nums_a)
 
-    even_index_num = nums_a.reverse_each.select.with_index { |_, i| i.odd? }
-    even_index_num = even_index_num.map { |n| n * 2 > 9 ? n * 2 - 9 : n * 2 }
-    even_total = even_index_num.sum
+    even_total = get_even_total(nums_a)
 
     ((odd_total + even_total) % 10).zero?
+  end
+
+  # Get total number of odd digits from reverse nums_a
+  def get_odd_total(nums_a)
+    odd_index_num = nums_a.reverse_each.select.with_index { |_, i| i.even? }
+
+    odd_index_num.sum
+  end
+
+  # Get total number of even digits from reverse nums_a
+  def get_even_total(nums_a)
+    even_index_num = nums_a.reverse_each.select.with_index { |_, i| i.odd? }
+    even_index_num = even_index_num.map { |n| n * 2 > 9 ? n * 2 - 9 : n * 2 }
+
+    even_index_num.sum
   end
 end
