d55c2dd445e41c497c731ab95ccb8f68cfccccf7
tienyulin <yh50114@gmail.com>
2021-03-19 08:56:03 +0000

    feat(cipher): implement caesar cipher

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -1,4 +1,5 @@
 module SubstitutionCipher
+  # Caesar Cipher
   module Caesar
     # Encrypts document using key
     # Arguments:
@@ -7,6 +8,11 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
+
+      encrypt_string = ''
+      document.to_s.each_byte { |s| encrypt_string += (s.ord + key).chr }
+
+      encrypt_string
     end
 
     # Decrypts String document using integer key
@@ -16,6 +22,11 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
+
+      decrypt_string = ''
+      document.to_s.each_byte { |s| decrypt_string += (s.ord - key).chr }
+
+      decrypt_string
     end
   end
 
